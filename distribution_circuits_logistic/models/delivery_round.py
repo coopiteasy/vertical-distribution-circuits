c8029b4064d98b48977c775b35d4b0df49598e3e
houssine <houssine.bakkali@gmail.com>
2017-03-07 23:21:36 +0100

    [IMP] adding delivery round model and link to picking and picking wave

---
+++ distribution_circuits_logistic/models/delivery_round.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from openerp import api, fields, models, _
+
+class DeliveryRound(models.Model):
+    _name = "delivery.round"
+    
+    name = fields.Char(string="Name", required=True)
+    time_frame_id = fields.Many2one('time.frame', string='Time frame', required=True)
+    delivery_date = fields.Date(related='time_frame_id.delivery_date', store=True)
+    lines = fields.One2many('delivery.round.line', 'delivery_round_id', string='Delivery line')
+    
+class DeliveryRoundLine(models.Model):    
+    _name = "delivery.round.line"
+    
+    sequence = fields.Integer(string="Sequence")
+    delivery_round_id = fields.Many2one('delivery.round', string="Delivery round", required=True)
+    raliment_point_id = fields.Many2one('res.partner', string="R'Aliment point", required=True)
+    picking_wave = fields.One2many('stock.picking.wave', 'round_line_id', string="Picking wave")
+    stock_picking = fields.One2many('stock.picking', 'round_line_id', string="Stock picking")
+    
--- distribution_circuits_logistic/models/stock.py
+++ distribution_circuits_logistic/models/stock.py
@@ -7,7 +7,14 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
     
     raliment_point = fields.Many2one("res.partner", string="Raliment Point", domain=[('is_raliment_point','=',True)])
+    round_line_id = fields.Many2one("delivery.round.line", string="Delivery round line")
+
+class StockPickingWave(models.Model):
     
+    _inherit = "stock.picking.wave"
+    
+    round_line_id = fields.Many2one("delivery.round.line", string="Delivery round line")
+        
 class StockPackOperation(models.Model):
     
     _inherit = "stock.pack.operation"
