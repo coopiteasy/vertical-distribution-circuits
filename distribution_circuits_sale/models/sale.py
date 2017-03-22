e6e4b13b32859aea3ef37813303c2f4874dfc5a9
houssine <houssine.bakkali@gmail.com>
2017-03-22 15:07:12 +0100

    [IMP] adds raliment point on the sale order

--- distribution_circuits_sale/models/sale.py
+++ distribution_circuits_sale/models/sale.py
@@ -6,4 +6,12 @@ class SaleOrder(models.Model):
     
     _inherit = "sale.order"
     
+    raliment_point = fields.Many2one(compute="_compute_raliment", comodel_name="res.partner", string="Raliment Point", domain=[('is_raliment_point','=',True)],store=True)
     time_frame_id = fields.Many2one('time.frame', string="Time Frame", required=True)
+    
+    @api.multi
+    @api.depends('partner_id')
+    def _compute_raliment(self):
+        for order in self:
+            if order.partner_id.parent_id and order.partner_id.parent_id.raliment_point:
+                order.raliment_point = order.partner_id.parent_id
