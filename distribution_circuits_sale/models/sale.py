eabe7b4fbb634f509b14afa4de40fc2c73b20155
houssine <houssine.bakkali@gmail.com>
2017-04-17 16:35:58 +0200

    [IMP] add related field uom and computed field supplier_id(first entry of supplier_info one2many relation)

--- distribution_circuits_sale/models/sale.py
+++ distribution_circuits_sale/models/sale.py
@@ -15,3 +15,17 @@ class SaleOrder(models.Model):
         for order in self:
             if order.partner_id.parent_id and order.partner_id.parent_id.raliment_point:
                 order.raliment_point = order.partner_id.parent_id
+
+class Product(models.Model):
+    
+    _inherit = "product.template"
+    
+    uom_name = fields.Char(related="uom_id.name", string="UoM Name")
+    supplier_id = fields.Many2one(compute="get_first_supplier", comodel_name="res.partner", string="Supplier")#, readonly=True, store=True 
+    
+    #@api.depends('seller_ids')
+    @api.multi
+    def get_first_supplier(self):
+        for seller in self.seller_ids:
+            self.supplier_id = seller.name
+            break
