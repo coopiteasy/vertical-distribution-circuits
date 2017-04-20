080b19cfa3618c97258207dac2eb2f5d3ee48589
houssine <houssine.bakkali@gmail.com>
2017-04-20 16:20:40 +0200

    [IMP] reduce copyright length

--- distribution_circuits_sale/__openerp__.py
+++ distribution_circuits_sale/__openerp__.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Coop IT Easy, Social Open Source Solution
-#    Copyright (C) 2017-2017 Coop IT Easy.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2017 Coop IT Easy (http://www.coopiteasy.be)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Short distribution circuits sale",
-    "version": "1.0",
+    'website': 'http://www.coopiteasy.be',
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
     "depends": ["distribution_circuits_base","sale"],
     "author": "Coop IT Easy - Houssine BAKKALI <houssine.bakkali@gmail.com>",
     "category": "Short distribution circuits",
