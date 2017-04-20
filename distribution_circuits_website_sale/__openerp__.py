ceb54b0dbf3b1a3bdb73eca8ea45ff20851c074c
houssine <houssine.bakkali@gmail.com>
2017-04-20 16:19:41 +0200

    [TYPO] fix typo

--- distribution_circuits_website_sale/__openerp__.py
+++ distribution_circuits_website_sale/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2016 Coop IT Easy (http://www.coopiteasy.be)
+# © 2017 Coop IT Easy (http://www.coopiteasy.be)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Distribution Circuits in E-Commerce',
     'category': 'e-commerce',
     'author': "Coop IT Easy - Houssine BAKKALI",
-    'website': 'http://www.coopiteasy',
+    'website': 'http://www.coopiteasy.be',
     'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
