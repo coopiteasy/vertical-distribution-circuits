bfdc4a709a21a7a7cc80530852f1179fbaf04c3b
houssine <houssine.bakkali@gmail.com>
2017-04-28 16:09:36 +0200

    [LIC] fix licensing

--- distribution_circuits_logistic/models/stock.py
+++ distribution_circuits_logistic/models/stock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+# © 2017 Houssine BAKKALI, Coop IT Easy
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import api, fields, models, _
 
 class StockPicking(models.Model):
