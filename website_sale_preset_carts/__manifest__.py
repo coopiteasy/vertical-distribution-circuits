cdddf137d01b54c6dee894b1d8e88d12834b435a
houssine <houssine.bakkali@gmail.com>
2020-02-27 11:22:17 +0100

    [IMP] add the option to block the order for insufficient credit.
    When the option is checked, if a subscriber doesn't have sufficient
    credit, the order is blocked. A list of blocked order is created and
    displayed in an other tab. Though, the user  can force the order
    creation if the time frame still open.

--- website_sale_preset_carts/__manifest__.py
+++ website_sale_preset_carts/__manifest__.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Coop IT Easy SCRLfs
 #     Robin Keunen <robin@coopiteasy.be>
+#     Houssine Bakkali <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Website Sale Preset Carts",
     "summary": "Allows the sale manager to preset weekly carts",
-    "version": "11.0.1.0.0",
+    "version": "11.0.2.0.0",
     "category": "Sales",
     "website": "https://github.com/coopiteasy/vertical-distribution-circuits",
     "author": "Coop IT Easy SCRL fs",
--- website_sale_preset_carts/models/res_partner.py
+++ website_sale_preset_carts/models/res_partner.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Coop IT Easy SCRLfs
 #     Robin Keunen <robin@coopiteasy.be>
+#     Houssine Bakkali <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
